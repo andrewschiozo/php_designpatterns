7c3d44f1b7881a38e5d55810a7f4a7e0c1903c89
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-13 14:56:47 -0300

    Factory de Automovel

--- factory/FactoryAutomovel.php
+++ factory/FactoryAutomovel.php
@@ -6,7 +6,13 @@
  * Date: 13/09/17
  * Time: 14:46
  */
+
+require_once 'Automovel.php';
+
 class FactoryAutomovel
 {
-
+    public static function create($marca, $modelo)
+    {
+        return new Automovel($marca, $modelo);
+    }
 }
