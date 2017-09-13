682b42ca8b79c410acf3408b4a1edea025b9f4e7
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-13 15:23:58 -0300

    Teste Database com padrao Singleton

--- singleton/index.php
+++ singleton/index.php
@@ -5,3 +5,17 @@
  * Date: 13/09/17
  * Time: 15:08
  */
+
+require_once 'DataBase.php';
+
+$Conexao = DataBase::getInstance();
+$OutraConexao = DataBase::getInstance();
+
+if($Conexao === $OutraConexao)
+{
+    echo 'Mesma instancia';
+}
+else
+{
+    echo 'Instancias diferentes';
+}
