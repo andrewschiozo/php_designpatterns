de9c10ae1c53c2128ea0d55c7204ed2cb1ac123f
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-13 15:22:49 -0300

    Classe de implementaçao Singleton

--- singleton/Singleton.php
+++ singleton/Singleton.php
@@ -8,5 +8,30 @@
  */
 class Singleton
 {
+    public static $instance = null;
 
+    public static function getInstance()
+    {
+        if(is_null(self::$instance))
+        {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+
+    final protected function __construct()
+    {
+
+    }
+
+    final protected function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    final protected function __wakeup()
+    {
+        // TODO: Implement __wakeup() method.
+    }
 }
