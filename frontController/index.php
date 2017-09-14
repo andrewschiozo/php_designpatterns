75847a82eaf8b1f63b5ae94980aa091aecc579d8
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-14 13:45:22 -0300

    Teste de FrontController

--- frontController/index.php
+++ frontController/index.php
@@ -5,3 +5,11 @@
  * Date: 14/09/17
  * Time: 09:45
  */
+
+require_once 'FrontController.php';
+
+$options = array('controller' => 'Usuario', 'method' => 'sayHello', 'params' => array('param1', 'param2', 'param3'));
+$fc = new FrontController($options);
+
+$fc->run();
+
