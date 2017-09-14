d0b3baa251a6dcd878a4cf25407d00220af62783
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-14 16:06:43 -0300

    Exemplos de chamadas do FronController

--- frontController/index.php
+++ frontController/index.php
@@ -8,8 +8,13 @@
 
 require_once 'FrontController.php';
 
-$options = array('controller' => 'Usuario', 'method' => 'sayHello', 'params' => array('param1', 'param2', 'param3'));
-$fc = new FrontController($options);
+//Chamada via codigo
+//$options = array('controller' => 'Usuario', 'method' => 'sayHello', 'params' => array('param1', 'param2', 'param3'));
+//$fc = new FrontController($options);
 
+
+//Chamada via URL
+//Exemplo de url:  http://www.meusistema.com.br/controller/metodo/param1/param2/paramN
+$fc = new FrontController();
 $fc->run();
 
