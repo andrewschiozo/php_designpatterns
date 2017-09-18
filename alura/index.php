aaedab4a726143854914c546ae0469273ab936e6
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-18 12:04:10 -0300

    Teste de Chain Of Responsability

--- alura/index.php
+++ alura/index.php
@@ -1,22 +1,38 @@
 <?php
+//Requires
 
-require_once 'CalculadoraDeImpostos.php';
+    //Impostos
+    require_once 'CalculadoraDeImpostos.php';
+    require_once 'ICMS.php';
+    require_once 'ISS.php';
+    require_once 'KCV.php';
 
-require_once 'ICMS.php';
-require_once 'ISS.php';
-require_once 'KCV.php';
+    //Descontos
+    require_once 'CalculadoraDeDescontos.php';
 
-require_once 'Orcamento.php';
+    //Entidade para teste
+    require_once 'Orcamento.php';
+    require_once 'Item.php';
 
+//Params
+$reforma = new Orcamento();
+$reforma->addItem(new Item('Tijolo', 50));
+$reforma->addItem(new Item('Cimento 1kg', 50));
+$reforma->addItem(new Item('Cimento 1kg', 100));
+$reforma->addItem(new Item('Cimento 1kg', 101));
 
-$reforma = new Orcamento(500);
-$calculadora = new CalculadoraDeImpostos();
+echo '<h3>Valor do orcamento: R$ ' . $reforma->getValor();
+echo '</h3><hr>';
 
-echo $calculadora->calcula($reforma, new ICMS);
-echo '<br>';
+$calculadoraDeImpostos = new CalculadoraDeImpostos();
+echo '<br>Impostos';
+echo '<br>ICMS: R$ ' . number_format($calculadoraDeImpostos->calcula($reforma, new ICMS),2,',', '.');
+echo '<br>ISS: R$ '  . number_format($calculadoraDeImpostos->calcula($reforma, new ISS),2,',', '.');;
+echo '<br>KCV: R$ '  . number_format($calculadoraDeImpostos->calcula($reforma, new KCV),2,',', '.');;
+echo '<hr>';
 
-echo $calculadora->calcula($reforma, new ISS);
-echo '<br>';
+//Descontos
+echo 'Descontos <br>';
 
-echo $calculadora->calcula($reforma, new KCV);
-echo '<br>';
+$calculadoraDeDescontos = new CalculadoraDeDescontos();
+echo '<br>Descontos: R$ ' . number_format($calculadoraDeDescontos->desconto($reforma), 2, ',', '.');
