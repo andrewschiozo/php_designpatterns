4fa28caae560320b7af0964a5b4e6821c5ccaf52
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-21 13:37:49 -0300

    Classe base para o builder

---
+++ alura/NotaFiscal.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: andrews
+ * Date: 21/09/17
+ * Time: 12:09
+ */
+class NotaFiscal
+{
+    private $empresa;
+    private $cnpj;
+    private $itens;
+    private $valorBruto;
+    private $valorImpostos;
+    private $observacoes;
+    private $dataEmissao;
+
+    public function __construct($nomeEmpresa, $cnpj, $listaItens, $valorBruto, $valorImpostos, $observacoes, $data)
+    {
+        $this->empresa = $nomeEmpresa;
+        $this->cnpj = $cnpj;
+        $this->itens = $listaItens;
+        $this->valorBruto = $valorBruto;
+        $this->valorImpostos = $valorImpostos;
+        $this->observacoes = $observacoes;
+        $this->dataEmissao = $data;
+    }
+
+    public function getEmpresa()
+    {
+        return $this->empresa;
+    }
+
+    public function getCnpj()
+    {
+        return $this->cnpj;
+    }
+}
