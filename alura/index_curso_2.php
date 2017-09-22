9bb41a6bdcf23d40ff3c957adc10fcff302eba89
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-22 10:56:26 -0300

    Teste do padrao Memento

--- alura/index_curso_2.php
+++ alura/index_curso_2.php
@@ -5,7 +5,25 @@ require_once 'AutoLoader.php';
 new AutoLoader;
 
 use Factory\FactoryConnection;
+use Memento\Contrato;
+use Memento\Historico;
 
+//Factory
 $factory = new FactoryConnection();
 $conexao = $factory->getConnection();
-var_dump($conexao);
+
+//Memento
+$historico = new Historico();
+$contrato = new Contrato('Caelum', new \Datetime);
+var_dump($contrato);
+
+$historico->addEstado($contrato->salvaEstado());
+$contrato->avanca();
+var_dump($contrato);
+
+$historico->addEstado($contrato->salvaEstado());
+$contrato->avanca();
+var_dump($contrato);
+
+var_dump($historico->getEstado(0));
+var_dump($historico->getEstado(1));
