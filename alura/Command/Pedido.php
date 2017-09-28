b3b8086e77c24bced1361a17a8c76becaf8efc95
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-28 15:22:42 -0300

    Classe pedido para implementaçao do pattern Command

---
+++ alura/Command/Pedido.php
@@ -0,0 +1,40 @@
+<?php
+namespace Command;
+use State\PedidoFinalizado;
+use State\PedidoNovo;
+use State\PedidoPago;
+
+/**
+ * Created by PhpStorm.
+ * User: andrews
+ * Date: 28/09/17
+ * Time: 14:48
+ */
+class Pedido
+{
+    private $cliente;
+    private $valor;
+    private $status;
+
+    public function __construct($cliente, $valor)
+    {
+        $this->cliente = $cliente;
+        $this->valor = $valor;
+        $this->status = new PedidoNovo();
+    }
+
+    public function pagar()
+    {
+        $this->status = new PedidoPago();
+    }
+
+    public function finalizar()
+    {
+        $this->status = new PedidoFinalizado();
+    }
+
+    public function getCliente()
+    {
+        return $this->cliente;
+    }
+}
---
+++ alura/State/iEstadoDeUmPedido.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrews
+ * Date: 28/09/17
+ * Time: 14:49
+ */
+
+namespace State;
+
+
+class iEstadoDeUmPedido
+{
+
+}
