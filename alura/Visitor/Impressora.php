8bf68451ef7ffa47b8c3e24afc9596ace87c0b3d
Andrews Chiozo <andrewschiozo@gmail.com>
2017-09-28 13:57:38 -0300

    Implementaçao do pattern Visitor com Impressora para a calculadora que usa Interpreter

---
+++ alura/Visitor/Impressora.php
@@ -0,0 +1,38 @@
+<?php
+namespace Visitor;
+/**
+ * Created by PhpStorm.
+ * User: andrews
+ * Date: 28/09/17
+ * Time: 13:35
+ */
+use Interpreter\Numero;
+use Interpreter\Soma;
+use Interpreter\Subtracao;
+
+class Impressora
+{
+
+    public function visitaSoma(Soma $soma)
+    {
+        echo "(";
+        $soma->getEsquerdo()->aceita($this);
+        echo "+";
+        $soma->getDireito()->aceita($this);
+        echo ")";
+    }
+
+    public function visitaSubtracao(Subtracao $subtracao)
+    {
+        echo "(";
+        $subtracao->getEsquerdo();
+        echo "-";
+        $subtracao->getDireito();
+        echo ")";
+    }
+
+    public function visitaNumero(Numero $numero)
+    {
+        echo $numero->getNumero();
+    }
+}
